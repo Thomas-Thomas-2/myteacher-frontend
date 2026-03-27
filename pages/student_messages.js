8f2944ac2126464cd8505227456ca46b8d3399ef
Thomas MENAGE <thomas.menage@hei.fr>
2026-03-27 12:22:29 +0100

    removing useless pages

--- pages/student_messages.js
+++
@@ -1,5 +0,0 @@
-import StudentMessages from "../components/StudentMessages";
-
-export default function StudentMessagesPage() {
-  return <StudentMessages />;
-}
--- pages/teacher_messages.js
+++
@@ -1,5 +0,0 @@
-import TeacherMessages from "../components/TeacherMessages";
-
-export default function TeacherMessagesPage() {
-  return <TeacherMessages />;
-}
